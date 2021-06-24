908b98642fbcca19a1ea6562e39adf7060128578
DizzyduckAR <52171360+DizzyduckAR@users.noreply.github.com>
2021-06-24 02:17:08 -0700

    Source Codes Folder. with ready to go batch file.
    venv.bat will open local venv and install all needed libs

---
+++ Source Codes/BotPlace Hub/client_config.py
@@ -0,0 +1,8 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'e77zFcVs+KNjuElOd8e60JOdILjp82YX263jY4o8/K8'
+    APP_NAME = 'BotPlace Hub'
+    COMPANY_NAME = 'Crystal Clear'
+    APP_VERSION = '1.0.0'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['https://raw.githubusercontent.com/DizzyduckAR/BotPlace/master/Update%20Hub/']
---
+++ Source Codes/BotPlace Hub/venv.bat
@@ -0,0 +1,3 @@
+python -m venv env
+CALL env\Scripts\activate.bat
+pip install -r requirements.txt
